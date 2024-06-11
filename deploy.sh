55d3de06c506357e0fd1c0134145000a200ee093
Eduardo Botelho <eduardo11224b@gmail.com>
2024-06-11 13:37:55 +0100

    fix script logic

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 # Function to check if a container exists by name
 container_running() {
   container_name=$1
-  if docker inspect ${container_name} -f {{.State.Running}}; then
+  container_status=$(docker inspect -f '{{.State.Running}}' ${container_name} 2>/dev/null)
+  if["$container_status" == "true"]; then
     echo "Container '$container_name' exists."
     return 0
   else
