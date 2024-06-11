6e57678a77dc9a06e5f5ad94b245083e963d1753
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2024-06-11 13:26:33 +0100

    deploy script test

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# Function to check if a container exists by name
+container_running() {
+  container_name=$1
+  if docker inspect ${container_name} -f {{.State.Running}}; then
+    echo "Container '$container_name' exists."
+    return 0
+  else
+    echo "Container '$container_name' does not exist."
+    return 1
+  fi
+}
+
+container_name="portainer"
+if container_exists "$container_name"; then
+  echo "Proceeding with the existing container..."
+else
+  echo "Creating a new container..."
+  # Add commands to create or start the container
+fi
